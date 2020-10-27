3b3d890b80bd07b106ad3ec9becb789aaca3a12a
Bucephalus <bhargabnath691@gmail.com>
2020-10-27 22:38:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+// const url = 'mongodb://localhost/ssnb';
+const url = 'mongodb+srv://amare:amare@mycluster.0e2la.gcp.mongodb.net/ssnb?retryWrites=true&w=majority';
+
+mongoose
+    .connect(
+        url,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRoutes = require('./routes/index');
+
+app.use('/', indexRoutes);
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+    console.log(`Server started on port ${PORT}`);
+});
