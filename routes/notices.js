36e4a27c015005968c571ab94507ad456007790c
Bucephalus <bhargabnath691@gmail.com>
2020-11-19 02:14:56 +0530

    gallery file updated

--- routes/notices.js
+++ routes/notices.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Notice = require('../models/notice');
-const {requireAuth} = require('../middleware/auth');
+const { requireAuth } = require('../middleware/auth');
 
-router.get('/list', requireAuth, function (req, res) {
-    Notice.find({}).exec(function (err, notices) {
+router.get('/list', function(req, res) {
+    Notice.find({}).exec(function(err, notices) {
         res.render('notices', {
             notices: notices,
         });
